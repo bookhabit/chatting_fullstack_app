1bae2f8e9b44ea16097c095b139223d4ef6e34dd
bookhabit <qnrgoqlt@naver.com>
2023-06-13 21:39:46 +0900

    메세지 송신 후 스크롤 처리

--- front/src/Chat.jsx
+++ front/src/Chat.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react"
+import { useContext, useEffect, useRef, useState } from "react"
 import Avatar from "./Avatar";
 import Logo from "./Logo";
 import { UserContext } from "./UserContext";
@@ -11,6 +11,7 @@ export default function Chat(){
     const [newMessageText,setNewMessageText] = useState('')
     const [messages,setMessages] = useState([]);
     const {username,id} = useContext(UserContext);
+    const divUnderMessages = useRef();
 
     useEffect(()=>{
         const ws = new WebSocket('ws://localhost:4000')
@@ -57,12 +58,17 @@ export default function Chat(){
           }]));
     }
 
+    useEffect(()=>{
+        const div = divUnderMessages.current;
+        if(div){
+            div.scrollIntoView({behavior:'smooth', block:'end'});
+        }
+    },[messages])
+
     const onlinePeopleExcludLoginUser = {...onlinePeople}
     delete onlinePeopleExcludLoginUser[id]
 
-    const messagesWithOutDupes = uniqBy(messages,'id');
-    console.log(messagesWithOutDupes)
-
+    const messagesWithoutDupes = uniqBy(messages, '_id');
     return(
         <div className="flex h-screen">
             {/* 웹소켓에 연결된 온라인 유저 목록  */}
@@ -91,14 +97,19 @@ export default function Chat(){
                         </div>
                     )}
                     {!!selectedUserId && (
-                        <div className="">
-                            {messagesWithOutDupes.map((message,index)=>(
-                                <div key={index} className={"p-2 "+(message.sender===id?'bg-blue-500 text-white':'bg-white text-gray-500')}>
-                                    sender: {message.sender}<br/>
-                                    my id : {id} <br/>
+                        <div className="mb-4 h-full">
+                            <div className="relative h-full">
+                                <div erf className="overflow-y-scroll absolute top-0 left-0 right-0 bottom-2">
+                            {messagesWithoutDupes.map(message => (
+                                <div key={message._id} className={(message.sender === id ? 'text-right': 'text-left')}>
+                                <div className={"text-left inline-block p-2 my-2 rounded-md text-sm " +(message.sender === id ? 'bg-blue-500 text-white':'bg-white text-gray-500')}>
                                     {message.text}
                                 </div>
+                                </div>
                             ))}
+                            <div ref={divUnderMessages}></div>
+                            </div>
+                        </div>
                         </div>
                     )}
                 </div>
